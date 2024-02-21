c252cd7b732c6e3db30c7490e62a23348b10d54f
KDHwan <litmu@naver.com>
2024-02-21 19:50:47 +0900

    타이틀 씬의 Help 기능 추가
    간단한 게임 설명

--- Assets/_Scripts/Manager/TitleSceneManager.cs
+++ Assets/_Scripts/Manager/TitleSceneManager.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleScene : MonoBehaviour
 {
+    public Sprite[] guide;
+    public int guideIndex;
+    public GameObject guideUI;
+    public GameObject guideSprite;
+
     public void GameStart()
     {
         SceneManager.LoadScene("MainScene");
@@ -13,5 +19,27 @@ public class TitleScene : MonoBehaviour
     public void GameOver()
     {
         Application.Quit();
+    }
+
+    public void GuideUIOpen()
+    {
+        guideUI.SetActive(true);
+    }
+
+    public void GuideUINext()
+    {
+        if (guideIndex < guide.Length)
+            guideIndex++;
+        else
+            return;
+
+        guideSprite.GetComponent<Image>().sprite = guide[guideIndex];
+    }
+
+    public void GuideUIClose()
+    {
+        guideUI.SetActive(false);
+        guideIndex = 0;
+        guideSprite.GetComponent<Image>().sprite = guide[guideIndex];
     }
 }
